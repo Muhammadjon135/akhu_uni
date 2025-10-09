3633abc08902e3def43ccc649d4899c296823ae6
Muhammadjon135 <117313935+Muhammadjon135@users.noreply.github.com>
2025-10-09 10:35:55 +0500

    Update second.py

--- second.py
+++ second.py
@@ -1,11 +1,17 @@
 ##Start
 first_dish = input("first dish name?\n")
 first_price = int(input("What is the price of the dish?\n"))
+how_many_first = int(input("How many meals?\n"))
+
 second_dish = input("second dish name?\n")
 second_price = int(input("What is the price of second the dish?\n"))
+how_many_second = int(input("How many meals?\n"))
+
 third_dish = input("third dish name?\n")
 third_price = int(input("What is the price of the third dish?\n"))
-original_price = first_price + second_price + third_price
+how_many_third = int(input("How many meals?\n"))
+
+original_price = how_many_first*first_price + how_many_second*second_price + how_many_third*third_price
 ## Customer info
 name = input("What is your name\n")
 student_status = input("Do you have a student ID(yes/no)\n")
@@ -16,7 +22,6 @@ is_large = original_price >= 150000
 large_discount = original_price*0.95*is_large
 print(large_discount, "Large discount")
 student_discount = original_price*0.85*is_student 
-print(student_discount, "student discount")
 happy_hour = order_time >= 14 and order_time <= 17
 main_discount = student_discount * (student_discount >= happy_hour) + happy_hour * (happy_hour > student_discount)
 print(main_discount, "Main discount")
